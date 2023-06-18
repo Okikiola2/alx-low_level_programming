4aae60d7bed8ba1228e331c23e5701db9cb215da
Okikiola2 <okikiola.olaniyan@gmail.com>
2023-06-19 01:00:20 +0200

    print with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * program that prints using puts
+ *
+ * not allow to use printf to print
+ *
+ * program should end with 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
+
