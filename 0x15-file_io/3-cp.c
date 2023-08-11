16a9a0307d347578c8e27cffe72a2d7e1bcae66a
Okikiola2 <okikiola.olaniyan@gmail.com>
2023-08-11 23:02:49 +0200

    a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -67,9 +67,9 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	buff = create_buff(argv[2]);
+	buff = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
+	r = read(from, buff, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
